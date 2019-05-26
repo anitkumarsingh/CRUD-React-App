4a229fb89a13aabbce58389378de3b3287b2b3cd
Anit Kumar <anikumarsingh99@gmail.com>
2019-05-26 12:14:09 +0530

    basic folder structure

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React,{ Fragment } from 'react';
+import { Header, Footer } from './Layouts';
+
+const App = () =>{
+    return(
+    <Fragment>
+        <Header/>
+
+        <Footer/>
+    </Fragment>
+                
+    )
+}
+export default  App;
