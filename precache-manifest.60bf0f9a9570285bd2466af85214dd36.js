ca1ef22308a188ca5c0126401e2555a9eda84bdf
Anit Kumar <anikumarsingh99@gmail.com>
2019-06-25 12:02:48 +0530

    Updates

---
+++ precache-manifest.60bf0f9a9570285bd2466af85214dd36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b59c6810c5f46b3124dbe9631d4492a",
+    "url": "/CRUD-React-App/index.html"
+  },
+  {
+    "revision": "bcf86a189c3c9637e957",
+    "url": "/CRUD-React-App/static/css/main.09431ba1.chunk.css"
+  },
+  {
+    "revision": "e9ad5498901a2f283ab5",
+    "url": "/CRUD-React-App/static/js/2.42d5eed0.chunk.js"
+  },
+  {
+    "revision": "bcf86a189c3c9637e957",
+    "url": "/CRUD-React-App/static/js/main.b373c6a0.chunk.js"
+  },
+  {
+    "revision": "e71c2ef27abe557b9f56",
+    "url": "/CRUD-React-App/static/js/runtime~main.8a7bbb38.js"
+  }
+]);
